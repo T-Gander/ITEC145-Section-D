8564933786796b09ba482b6c7798cce75e6897d5
Thomas Gander (Home) <Thomas.Gander@mymhc.ca>
2023-01-28 17:30:58 -0700

    Attempting to do structs but VSStudio is bugging out and not showing me errors, will try again later

--- ITEC145-Section-D/Form1.cs
+++ ITEC145-Section-D/Form1.cs
@@ -1,46 +1,69 @@
+using System.Globalization;
+
 namespace ITEC145_Section_D
 {
     public partial class Form1 : Form
     {
         string fileRecords = "";
         List<string> columnNames = new List<string> {"Make", "Model", "Year", "Mileage" };
-        List<string> columnValues = new List<string>();     //Need to change this to a struct
-        int columns = 0;
-        struct cars
-        {
+        List<string> allCars = new List<string>();     //Need to change this to a struct
+        
 
+        
+    struct cars
+        {
+            public string Make;
+            public string Model;
+            public int Year;
+            public int Mileage;
         }
         
 
         public Form1()
         {
             InitializeComponent();
+            dataGridCars.ColumnCount = columnNames.Count;               //Sets and creates the amount of columns that are needed.
+            for (int i = 0; i < dataGridCars.ColumnCount; i++)          //Loop to add each column with the names specified in the columnNames list.
+            {
+                dataGridCars.Columns[i].HeaderText = columnNames[i];    //Sets each columns name based on the names in columnNames
+            }
         }
 
         private void btnLoad_Click(object sender, EventArgs e)
         {
-            dataGridCars.ColumnCount = columnNames.Count;    //Sets and creates the amount of columns that are needed.
-            for (int i = 0; i < columns; i++)                //Loop to add each column with the names specified in the columnNames list.
-            {
-                dataGridCars.Columns[i].Name = columnNames[i];  //Sets each columns name based on the names in columnNames
-            }
-
             OpenFileDialog ofd = new OpenFileDialog();
             //ofd.ShowDialog();
             
             if(ofd.ShowDialog() == DialogResult.OK)                     //If file dialog is successful in selecting a file. Do something.
             {
-                int count = 0;                                          //To keep count of records read.
+                
+                int count = 0;                                                       //To keep count of records read.
                 StreamReader reader = new StreamReader(ofd.FileName);
+                var[] words = new var[columnNames.Count];
 
-                for(int i = 0; !reader.EndOfStream; i++)
+
+                for(int i=0; !reader.EndOfStream, i++)
                 {
-                    columnValues.Add(reader.ReadLine());
-                    count++;
+                    cars carList;
+                    words = reader.ReadLine().Split(",");
+                    for(int j = 0; j < words.Length; j++)
+                    {
+                        carList[j] == words[j];
+                    }
+
+                    if (i == 3)
+                        i = 0;
                 }
+
+
+                //for(int i = 0; !reader.EndOfStream; i++)
+                //{
+                //    columnValues.Add(reader.ReadLine());
+                //    count++;
+                //}
                 reader.Close();
 
-               txtFileSelect.Text = $"{count} records were added to the List";           
+               txtFileSelect.Text = $"{count} records were added to the List";  
             }
             else
             {
